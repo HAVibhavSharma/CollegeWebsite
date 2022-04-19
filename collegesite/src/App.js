e6f5cdf1d94482111a805e4b71ccaf51ccd299a9
Vibhav <vibhavsharmaha@gmail.com>
2022-04-19 02:17:06 -0700

    Start

---
+++ collegesite/src/App.js
@@ -0,0 +1,11 @@
+import NavBar from "./Components/NavBar";
+import 'bootstrap/dist/css/bootstrap.css';
+function App() {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
